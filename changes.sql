723283ab6216f14e76c1719ae13b4483390481eb
DanielTkachenko <pdg.danila.98@mail.ru>
2020-07-23 16:04:48 +0700

    Changed User table / changed type of Sex column

--- changes.sql
+++ changes.sql
@@ -1,29 +1,14 @@
 use TPU_Project
 GO
---19.07.20-----------------------------------
---в таблицу электронная почта устанавливаем уникальность для имени электронной почты
-alter table [Электронная почта]
-add constraint EMAIL_UNIQUE unique([Наименование])
-go
---переделываем таблицу Пользователь / добавляем колонку для указания типа регистрации и удаляем колонку Логин
+--23.07.20-----------------------------------
+
+--переделываем таблицу Пользователь / меняем тип колонки Пол
 alter table [Пользователь]
-drop column [Логин]
+alter column [Пол] varchar(10) not null
 go
-alter table [Пользователь]
-add [Provider] varchar(30) not null
-go
-ALTER VIEW UserInfo AS
-SELECT Us.[Id Пользователя], Us.Фамилия, Us.Имя, Us.Отчество, Us.Роль, Us.Пол, Языки.Наименование 'Язык', 
-Em.Наименование 'Электронная почта', Num.[Номер телефона], Us.Пароль, Us.[refresh salt], Us.[Provider]
-FROM Пользователь Us INNER JOIN Языки ON Us.[ID языка] = Языки.[ID языка]
-LEFT JOIN [Электронная почта] Em
-ON Us.[Id Пользователя] = Em.[Id Пользователя]
-LEFT JOIN [Номера телефонов] Num ON Us.[Id Пользователя] = Num.[Id Пользователя]
-go
---убираем из процедуры AddUser параметр Логин и добавляем параметр Provider
 ALTER PROCEDURE [AddUser]
 @Provider varchar(30), @Password varchar(200), @FirstName varchar(45), @SecondName varchar(45) = null, 
-@Patronymic varchar(45) = NULL, @Sex bit, @Role varchar(45), @Language varchar(45), @Email varchar(45), @PhoneNumber varchar(11)=NULL
+@Patronymic varchar(45) = NULL, @Sex varchar(10), @Role varchar(45), @Language varchar(45), @Email varchar(45), @PhoneNumber varchar(11)=NULL
 AS
 DECLARE @IdLaguage uniqueidentifier, @UserId uniqueidentifier
 SELECT @IdLaguage = Языки.[ID языка]
@@ -37,17 +22,3 @@ END
 INSERT INTO [Электронная почта] VALUES (NEWID(), @Email, @UserId)
 INSERT INTO Пользователь([Id Пользователя], Фамилия, Имя, Отчество, Роль, Пол, [ID языка], Пароль, [Provider])
 VALUES(@UserId, @SecondName,@FirstName,@Patronymic,@Role,@Sex,@IdLaguage, @Password, @Provider)
-go
---редактируем ХП EditUserRefreshSalt / на вход подается email
-ALTER procedure [EditUserRefreshSalt]
-@Salt varchar(50), @email varchar(30)
-as
-update [Пользователь] 
-set [refresh salt] = @Salt
-from (select Us.[Id Пользователя], Us.[refresh salt] 
-from [Пользователь] Us join [Электронная почта] Em on Us.[Id Пользователя]=Em.[Id Пользователя]
-where [Наименование] = @email) as Selected
-where Пользователь.[Id Пользователя] = Selected.[Id Пользователя]
-go
---удалить ХП GetUserByLogin
-drop procedure GetUserByLogin
