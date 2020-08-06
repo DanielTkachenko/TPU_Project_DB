8545bea6fce0376aff9534852328f89a849944f8
DanielTkachenko <pdg.danila.98@mail.ru>
2020-08-06 14:03:18 +0700

    Pictures were аdded to menu items

--- changes.sql
+++ changes.sql
@@ -1,24 +1,64 @@
 use TPU_Project
 GO
---23.07.20-----------------------------------
+--06.08.20-----------------------------------
 
---переделываем таблицу Пользователь / меняем тип колонки Пол
-alter table [Пользователь]
-alter column [Пол] varchar(10) not null
+--добавляем в таблицу Представление меню колонку для картинок
+alter table [Пункт меню]
+add [ID картинки] uniqueidentifier null
 go
-ALTER PROCEDURE [AddUser]
-@Provider varchar(30), @Password varchar(200), @FirstName varchar(45), @SecondName varchar(45) = null, 
-@Patronymic varchar(45) = NULL, @Sex varchar(10), @Role varchar(45), @Language varchar(45), @Email varchar(45), @PhoneNumber varchar(11)=NULL
+alter table [Пункт меню]
+add foreign key([ID картинки]) references [Медиа]([Id Медиа])
+go
+
+--корректируем ХП для вставки
+ALTER PROCEDURE [dbo].[AddMenuItem]
+@Level int,@NameMenu varchar(45), @NameUpperMenu varchar(45), @Language varchar(45), 
+@Order int, @type varchar(20), @url varchar(100), @img uniqueidentifier = null
 AS
-DECLARE @IdLaguage uniqueidentifier, @UserId uniqueidentifier
-SELECT @IdLaguage = Языки.[ID языка]
+DECLARE @MenuId uniqueidentifier
+DECLARE @UpperMenuId uniqueidentifier
+DECLARE @LanguageId uniqueidentifier
+SELECT @MenuId = NEWID()
+SELECT @LanguageId=Языки.[ID языка]
 FROM Языки
 WHERE Языки.Наименование = @Language
-SELECT  @UserId=NEWID()
-IF @PhoneNumber IS NOT NULL
+IF @Level!=1
 BEGIN
-	INSERT INTO [Номера телефонов] VALUES (NEWID(), @PhoneNumber,@UserId)
+IF @NameUpperMenu is not NULL
+BEGIN
+SELECT @UpperMenuId = [Пункт меню].[Id пункта меню]
+FROM [Пункт меню] INNER JOIN [Представление меню]
+ON [Пункт меню].[Id пункта меню] = [Представление меню].[Id пункта меню]
+INNER JOIN Языки ON [Представление меню].[ID языка] = Языки.[ID языка]
+WHERE [Представление меню].Наименование = @NameUpperMenu AND Языки.Наименование=@Language
+IF @UpperMenuId is not NULL
+BEGIN
+INSERT INTO [Пункт меню]([Id пункта меню],Позиция,[FK_Id пункта меню], [Порядок отображения], [Тип меню], [URL], [ID картинки])
+VALUES(@MenuId,@Level,@UpperMenuId, @Order, @type, @url, @img)
+INSERT INTO [Представление меню]([Id пункта меню],Наименование,[ID языка])
+VALUES(@MenuId,@NameMenu,@LanguageId)
 END
-INSERT INTO [Электронная почта] VALUES (NEWID(), @Email, @UserId)
-INSERT INTO Пользователь([Id Пользователя], Фамилия, Имя, Отчество, Роль, Пол, [ID языка], Пароль, [Provider])
-VALUES(@UserId, @SecondName,@FirstName,@Patronymic,@Role,@Sex,@IdLaguage, @Password, @Provider)
+ELSE
+print 'No such upper menu item'
+END
+ELSE
+print 'Error no upper menu name'
+END
+IF @Level=1
+BEGIN
+INSERT INTO [Пункт меню]([Id пункта меню],Позиция, [Порядок отображения], [Тип меню], [URL], [ID картинки])
+VALUES(@MenuId,@Level, @Order,@type, @url, @img)
+INSERT INTO [Представление меню]([Id пункта меню],Наименование,[ID языка])
+VALUES(@MenuId,@NameMenu,@LanguageId)
+END;
+go
+--редактируем представление меню
+ALTER VIEW Menu AS
+SELECT P1.[Id пункта меню] 'ID', Pr1.Наименование 'Подчинённый', P1.Позиция 'Уровень меню', Jz1.Наименование 'Язык подчинённого' ,P1.[Тип меню] 'Тип', 
+P1.[URL] 'Ссылка', P1.[Порядок отображения], SP1.[ID статьи] 'ID статьи', P1.[ID картинки] 'ID картинки', P2.[Id пункта меню] 'ID родителя', Pr2.Наименование 'Родитель'
+FROM ([Пункт меню] P1 
+INNER JOIN [Представление меню] Pr1 ON P1.[Id пункта меню] = Pr1.[Id пункта меню]
+LEFT JOIN [Статьи пункты меню] SP1 ON SP1.[ID пункта меню] = P1.[Id пункта меню]
+INNER JOIN Языки Jz1 ON Pr1.[ID языка] = Jz1.[ID языка])
+LEFT JOIN ([Пункт меню] P2 INNER JOIN [Представление меню] Pr2 ON P2.[Id пункта меню]=Pr2.[Id пункта меню])
+ON P1.[FK_Id пункта меню] = P2.[Id пункта меню]
