e4c09403bc0564f66c3a079a470221913af3b570
DanielTkachenko <pdg.danila.98@mail.ru>
2020-08-28 14:33:39 +0700

    Procedure EditUser was edited

--- changes.sql
+++ changes.sql
@@ -73,3 +73,31 @@ INSERT INTO [Статьи пункты меню]([ID пункта меню], [ID статьи])
 VALUES(@MenuId, @articleId)
 END
 END;
+GO
+--28.08.20
+--EDIT PROCEDURE EDITUSER
+ALTER PROCEDURE [dbo].[EditUser]
+@email varchar(100), @psw varchar(100), @firstName varchar(50), @lang varchar(20), @secondName varchar(50) = null, @patronymic varchar(50) = null,
+@sex varchar(20) = null, @phoneNum varchar(20) = null
+AS
+DECLARE @langid uniqueidentifier, @userid uniqueidentifier
+--находим id языка по наименованию
+SET @langid = (SELECT [ID языка] FROM [Языки] WHERE [Наименование] = @lang)
+--находим id пользователя по email
+SET @userid = (SELECT Us.[Id Пользователя] 
+FROM [Пользователь] Us JOIN [Электронная почта] Em ON Us.[Id Пользователя] = Em.[Id Пользователя] 
+WHERE Em.Наименование = @email)
+UPDATE [Пользователь]
+SET Пароль = @psw, Имя = @firstName, Фамилия = @secondName, Отчество = @patronymic, Пол = @sex, [ID языка] = @langid
+WHERE [Id Пользователя] = @userid;
+IF @phoneNum != NULL
+BEGIN
+	UPDATE [Номера телефонов]
+	SET [Номер телефона] = @phoneNum
+	WHERE [Id Пользователя] = @userid
+END
+ELSE
+BEGIN
+	INSERT INTO [Номера телефонов]([Id Телефона], [Id Пользователя], [Номер телефона])
+	VALUES(NEWID(), @userid, @phoneNum)
+END
